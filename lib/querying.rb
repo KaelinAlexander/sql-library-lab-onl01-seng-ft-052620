f5121cf638cfdaa795721d31ebcfd89b6d597286
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 20:32:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,8 +30,8 @@ def select_series_title_with_most_human_characters
   JOIN characters ON characters.author_id = authors.id
   GROUP BY characters.species = 'human'
   JOIN character_books ON character_books.character_id = characters.id
-  JOIN books on character_books.book_id = books.id;"
-  ORDER BY series.title LIMIT 1
+  JOIN books on character_books.book_id = books.id
+  ORDER BY series.title LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
