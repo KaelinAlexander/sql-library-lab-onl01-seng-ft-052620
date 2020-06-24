f9c4db8917a657518cd43bf469b9b51bec312ab2
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 18:27:47 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,29 +1,29 @@
 CREATE TABLE series (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   author TEXT,
   sub_genre TEXT
 );
 
 CREATE TABLE sub_genres (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE authors (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT,
 );
 
 CREATE TABLE books (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
 );
 
 CREATE TABLE characters (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
   species TEXT,
@@ -31,7 +31,7 @@ CREATE TABLE characters (
 );
 
 CREATE TABLE character_books (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   character_id INTEGER,
   book_id INTEGER
 );
