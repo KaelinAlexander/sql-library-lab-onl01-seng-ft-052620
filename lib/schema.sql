c6e6f01f6a9ff4b573ca7f09cd83eca9457aff0b
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 18:31:36 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id TEXT,
-  sub_genre_id TEXT
+  subgenre_id TEXT
 );
 
-CREATE TABLE sub_genres (
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
