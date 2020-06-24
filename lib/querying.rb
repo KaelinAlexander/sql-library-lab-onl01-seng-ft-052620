4cdd1d8e33178176b837343c99d7c603b0d53307
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 20:08:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,14 +21,14 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
   JOIN series ON series.author_id = authors.id
-  JOIN subgenres ON subgenres.id = series.subgenre_id";
+  JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN authors ON authors.id = series.author_id
   JOIN characters ON characters.author_id = authors.id
-  COUNT(characters.species) WHERE characters.species = 'human'";
+  COUNT(characters.species) WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
