1afb826f351bd8965bdd98bd011d6cc4c47dfeb1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 18:36:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,11 +4,13 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES
 ;
 
 INSERT INTO subgenres (name)
-
+("Sub A")
+("Sub B")
 ;
 
 INSERT INTO authors (name)
-
+("Author A")
+("Author B")
 ;
 
 INSERT INTO books (title, year, series_id)
