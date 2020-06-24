3c8ec53ad595758068be00b9b506415c3d6f0218
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 19:18:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,9 +6,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto
+  "SELECT characters.name, characters.motto, DATALENGTH(characters.motto) AS motto_length
   FROM characters
-  DATALENGTH(characters.motto) AS motto_length
   GROUP BY motto_length DESC
   LIMIT 1;"
 end
