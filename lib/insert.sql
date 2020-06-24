95668870527ed66dfa5c382a52c8ef5e171e7d8f
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 18:45:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -34,12 +34,20 @@ INSERT INTO character (name, motto, species, author_id) VALUES
 ;
 
 INSERT INTO character_books (character_id, book_id) VALUES
-(1, )
-(2, )
-(3, )
-(4, )
-(5, )
-(6, )
-(7, )
-(8, )
+(1, 1)
+(1, 2)
+(1, 3)
+(2, 1)
+(2, 2)
+(2, 3)
+(3, 1)
+(4, 2)
+(5, 4)
+(5, 5)
+(5, 6)
+(6, 4)
+(6, 5)
+(6, 6)
+(7, 4)
+(8, 5)
 ;
