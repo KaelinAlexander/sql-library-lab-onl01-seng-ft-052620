14e96cefc01952c634185e2cacd767e6650979f1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 19:12:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
-  COUNT(characters.motto) as motto_length
+  COUNT(characters.motto) AS motto_length
   GROUP BY motto_length DESC
   LIMIT 1;"
 end
